e13f4265b4154ccc8aa96002a4db0f6aaa5898aa
Vladimir <emirwtf@gmail.com>
2020-02-29 22:20:57 +0200

    first commit

---
+++ app/main.js
@@ -0,0 +1,20 @@
+let 
+    main = document.querySelector('.main_side'),
+    bascet = document.querySelector('.bascet'),
+    shop = document.querySelector('.shop'),
+    card = document.querySelector('.card');
+    
+    
+    bascet.addEventListener('click',showBascet);
+    shop.addEventListener('click',createCard);
+
+
+    function showBascet(){
+        console.log('hola!');
+    }
+
+    function createCard(){       
+        
+        console.log('wattafack');
+        
+    }
